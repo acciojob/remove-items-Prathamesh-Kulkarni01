e67e3fed268b06f2eb4e0b03d558d70eceb017e5
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-29 13:50:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+const remove = function () {
+  let select = document.getElementById("colorSelect");
+  let val = select.value;
+
+  // Loop through the options in reverse order and remove the selected option
+  for (let i = select.options.length - 1; i >= 0; i--) {
+    if (select.options[i].innerText === val) {
+      select.remove(i);
+    }
+  }
+
+  console.log("Updated select:", select);
+};
